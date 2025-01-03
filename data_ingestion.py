e8c83c4b7b60dd475df20783060cca596720ddb8
Amari Bolmer <amarirules@gmail.com>
2025-01-02 17:01:17 -0800

    Testing file

--- data_ingestion.py
+++ data_ingestion.py
@@ -3,7 +3,7 @@ from orb import Orb
 from dotenv import load_dotenv
 
 load_dotenv()
-
+#Testing file to ensure connection to Orb. Disregard file
 client = Orb(
     api_key=os.environ.get("ORB_API_KEY"),  # This is the default and can be omitted
 )
